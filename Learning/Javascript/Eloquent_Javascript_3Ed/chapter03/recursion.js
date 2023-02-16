cc01447a330d6751d5f5929cfe9489f36ebe1e1a
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-02-16 21:26:33 +0100

    Corrected the solution in recursion.js 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter03/recursion.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter03/recursion.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter03/recursion.js
@@ -11,13 +11,26 @@ Define a recursive function isEven corresponding to this description. The functi
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
 */
 
-function isEven(num) {
+/* function isEven(num) {
     if (num < 0) {
         console.log("Only positive number is allowed!");
     } else if (num % 2 === 0) {
         return true;
     } else if (num % 2 === 1) {
         return false;
+    } else {
+        return isEven(num - 2);
+    }
+} */
+
+// Using solution from the book.
+function isEven(num) {
+    if (num === 0) {
+        return true;
+    } else if (num === 1) {
+        return false;
+    } else if (num < 0) {
+        return isEven(-num);
     } else {
         return isEven(num - 2);
     }
