6258b948b8791dfb5697c0f62bb9ffd85aaa41b6
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-02-27 22:56:40 +0100

    Added more comments to code 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter07/project-a_robot.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter07/project-a_robot.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter07/project-a_robot.js
@@ -23,6 +23,7 @@ function buildGraph(edges) {
             graph[from].push(to);
         }
     }
+    // Loop over an array containing a series of loop to add to the graph.
     for (let [from, to] of edges.map(r => r.split("-"))) {
         addEdge(from, to);
         addEdge(to, from);
@@ -30,6 +31,7 @@ function buildGraph(edges) {
     return graph;
 }
 
+// Read-only because of constant declaration and global scope
 const roadGraph = buildGraph(roads);
 /* console.log(roadGraph);
 console.log("\n=========+=========\n");
@@ -78,14 +80,23 @@ console.log(object.value); // → 5
 console.log("\n=========+=========\n");
  */
 
-// Dumb strategy for robot to randomly pick parcels and then deliver them
+// Dumb strategy for robot to randomly pick parcels and then deliver them.
 function randomPick(array) {
     let choice = Math.floor(Math.random() * array.length);
     return array[choice];
 }
+
 function randomRobot(state) {
     return {direction: randomPick(roadGraph[state.place])};
 }
+/* 
+runRobot(VillageState.random(), randomRobot);
+ */
+// → Moved to Marketplace
+// → Moved to Town Hall
+// → …
+// → Done in 70 turns
+console.log("\n=========+=========\n");
 
 // Create a new state with some parcels
 VillageState.random = function(parcelCount = 5) {
@@ -104,6 +115,7 @@ VillageState.random = function(parcelCount = 5) {
 
 // Robot must look at surrounding state and remember prior events
 function runRobot(state, robot, memory) {
+    // Continue with simulation until all the parcels have been delivered.
     for (let turn = 0;; turn++) {
         console.log(`\n<-- On turn ${turn} at ${state.place}: -->\n\nParcels:`);
         for (let parcel of state.parcels) {
@@ -116,6 +128,7 @@ function runRobot(state, robot, memory) {
             console.log(`Done in ${turn} turns!`);
             break;
         }
+
         let action = robot(state, memory);
         // console.log(`Action: ${action}`);
         state = state.move(action.direction);
@@ -125,16 +138,8 @@ function runRobot(state, robot, memory) {
 }
 console.log("\n=========+=========\n");
 
-/* 
-runRobot(VillageState.random(), randomRobot);
- */
-// → Moved to Marketplace
-// → Moved to Town Hall
-// → …
-// → Done in 70 turns
-console.log("\n=========+=========\n");
-
-// Using routes that the robot can transverse twice will be more efficient.
+// Using routes that pass through all places in the village so that the robot can transverse them twice will be more efficient (1st journey to pick all parcels, 2nd to deliver all of them).
+// Robot will take turns that is at maximum double the number of places.
 
 const mailRoute = [
     "Alice's House", "Cabin", "Alice's House", "Bob's House", "Town Hall", "Daria's House", "Ernie's House", "Grete's House", "Shop", "Grete's House", "Farm", "Marketplace", "Post Office"
@@ -155,13 +160,18 @@ console.log("\n=========+=========\n");
 // Pathfinding by searching for the shortest route between the destinations will improve the efficiency of the robot.
 // Places reached first are explored by means of a work list array via evenly crawling web/tree of known routes
 function findRoute(graph, from, to) {
+    // An array of places that should be explored next and the route that led there. Starts from the start with an empty route.
     let work = [{at: from, route: []}];
     for (let i = 0; i < work.length; i++) {
+        // Search using the next item in the work list.
         let {at, route} = work[i];
+        // Explore all reachable places via roads coming from "at" place.
         for (let place of graph[at]) {
+            // Return the route if one of them is the goal.
             if (place == to) {
                 return route.concat(place);
             }
+            // Otherwise add new item to work list if the place had not been checked.
             if (!work.some(w => w.at == place)) {
                 work.push({at: place, route: route.concat(place)});
             }
@@ -171,12 +181,26 @@ function findRoute(graph, from, to) {
 
 //
 function goalOrientedRobot({place, parcels}, route) {
-    // List of directions is empty
+    /*
+     * The robot tries to actively decide on the shortest route between places to pick up and deliver parcels.
+     *
+     * This robot uses its memory value as a list of directions to move in, just like the route-following robot. Whenever that list is empty, it has to figure out what to do next. 
+     * 
+     * It takes the first undelivered parcel in the set and, if that parcel hasn’t been picked up yet, plots a route toward it. 
+     * 
+     * If the parcel has been picked up, it still needs to be delivered, so the robot creates a route toward the delivery address instead.
+     *
+     * Returns an `action` object where `direction` property references the place where the robot will travel next and the `memory` property will reference an array representing the remaining places that needs to be visited in the route, after robot moves to the `direction` place.
+    */
+    
+    // If no route is decided, then find the shortest route to the pickup point of the first parcel in `parcels` (if its yet to be picked). Otherwise, if the parcel has already been picked, then find the shortest route to the parcel's address.
+    // 
+    // Skip the step of finding a route, if a robot is already travelling through a route.
     if (route.length == 0) {
         console.log("... Decision: Robot decides on a new route.\n");
         // Checks first parcel from its list
         let parcel = parcels[0];
-        // Plot route to pick up parcel, other plot route to deliver it
+        // Plot route to pick up parcel, otherwise plot route to deliver it
         if (parcel.place != place) {
             route = findRoute(roadGraph, place, parcel.place);
             console.log(`Route to PICK UP Parcel 1 at ${parcel.place}: ${route}.`);
