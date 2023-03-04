2ab27d992e8233aa0a217e652ed12a1af41c7ef4
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-04 14:25:45 +0100

    Added book solution to retry.js exercise in ch. 8

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter08/retry.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter08/retry.js
@@ -21,7 +21,7 @@ function primitiveMultiply(num1, num2) {
 function errorTester() {
     let counter = 0;
     let resultArray = [];
-    while (counter < 200) {
+    while (counter < 20) {
         try {
             resultArray.push(primitiveMultiply(10, 5));
             // resultArray.push(primitiveMultiply(10, "home"));
@@ -40,3 +40,26 @@ function errorTester() {
 // console.log(primitiveMultiply(5, 8));
 // console.log(errorTester());
 console.table(errorTester());
+
+/* Book solution */
+function primitiveMultiplyBook(a, b) {
+    if (Math.random() < 0.2) {
+        return a * b;
+    } else {
+        throw new MultiplicatorUnitFailure("Klunk");
+    }
+}
+
+function reliableMultiply(a, b) {
+    for (;;) {
+        try {
+            return primitiveMultiplyBook(a, b);
+        } catch (e) {
+            if (!(e instanceof MultiplicatorUnitFailure)) {
+                throw e;
+            }
+        }
+    }
+}
+console.log(reliableMultiply(8, 8)); // → 64
+console.log(reliableMultiply(8, "dud"));
