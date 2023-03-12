459babd1cb7619d934f9592a4abd78dc33341d1b
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-13 00:17:58 +0100

    Buggy solution 	modified:   Learning/Javascript/The_Javascript_Way/chapter16/js/changing_colours.js

--- Learning/Javascript/The_Javascript_Way/chapter16/js/changing_colours.js
+++ Learning/Javascript/The_Javascript_Way/chapter16/js/changing_colours.js
@@ -1,24 +1,26 @@
 /* Write the associated JavaScript code that updates the background colour of all div tags according to the key (R, Y, G or B) pressed by the user.
 */
 
-let keyCode = "";
-
+let backgroundColour = "";
 document.addEventListener("keypress", event => {
-    keyCode = `${String.fromCharCode(event.charCode)}`;
-    return keyCode;
-});
-
-const divElements = document.querySelectorAll("div");
-
-divElements.forEach(item => {
-    if (keyCode === "red" || keyCode === "R" || keyCode === "r") {
-        item.backgroundColor = red;
-    } else if (keyCode === "green" || keyCode === "G" || keyCode === "g") {
-        item.backgroundColor = green;
-    } else if (keyCode === "blue" || keyCode === "B" || keyCode === "b") {
-        item.backgroundColor = blue;
+    let code = String.fromCharCode(event.charCode);
+    console.log(code);
+    
+    if (code === "red" || code === "R" || code === "r") {
+        backgroundColour = "red";
+    } else if (code === "green" || code === "G" || code === "g") {
+        backgroundColour = "green";
+    } else if (code === "blue" || code === "B" || code === "b") {
+        backgroundColour = "blue";
     } else {
         console.error("Invalid keypress");
     }
+    // return backgroundColour;
+});
+
+const divElements = Array.from(document.getElementsByTagName("div"));
+
+divElements.forEach(item => {
+    item.style.backgroundColor = backgroundColour;
 });
 
