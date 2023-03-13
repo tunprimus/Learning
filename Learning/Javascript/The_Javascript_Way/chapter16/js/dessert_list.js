2c4ed79839096c07c9172b2f9b685e6668d58d24
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-13 09:32:33 +0100

    Ongoing solution to "dessert list" exercise Ch. 16 	new file:   Learning/Javascript/The_Javascript_Way/chapter16/html/dessert_list.html 	new file:   Learning/Javascript/The_Javascript_Way/chapter16/js/dessert_list.js

---
+++ Learning/Javascript/The_Javascript_Way/chapter16/js/dessert_list.js
@@ -0,0 +1,14 @@
+/* Write the JavaScript code that adds a new dessert to the list when the user clicks on the button. The dessert name is chosen by the user.
+Bonus points for adding the possibility of changing a dessert’s name when clicking on it.
+*/
+
+/*
+- add event listener to button
+- display prompt for user to enter dessert name and store it
+- create list item and use stored name as text content
+- add event listener to to generated list
+*/
+
+const addBtnElement = document.getElementById("addButton");
+
+const listsElement = document.getElementById("desserts");
