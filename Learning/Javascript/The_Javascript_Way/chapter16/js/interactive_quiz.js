9247db91bcf4ea6b4341271734561c364dbb8605
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-13 16:12:41 +0100

    new file:   Learning/Javascript/The_Javascript_Way/chapter16/html/interactive_quiz.html 	new file:   Learning/Javascript/The_Javascript_Way/chapter16/js/interactive_quiz.js

---
+++ Learning/Javascript/The_Javascript_Way/chapter16/js/interactive_quiz.js
@@ -0,0 +1,21 @@
+/* Complete this code to display the questions in the <div> element of the page, with a "Show the answer" button next to each question. Clicking this button replaces it with the answer for this question.
+*/
+
+const questions = [
+    {
+        statement: "2+2?",
+        answer: "2+2 = 4"
+    },
+    {
+        statement: "In which year did Christopher Columbus discover America?",
+        answer: "1492"
+    },
+    {
+        statement: "What occurs twice in a lifetime, but once in every year, twice in a week but never in a day?",
+        answer: "The E letter"
+    }
+];
+
+questions.forEach(item => {
+    const quizElement = document.getElementById("content");
+});
