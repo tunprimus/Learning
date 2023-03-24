5d22569c3c655c38fc4774e6a74b517d9b2c9820
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-24 17:20:17 +0100

    Book examples in Ch. 15 	new file:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.html 	new file:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js

---
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js
@@ -0,0 +1,61 @@
+//
+
+
+/* Event Handlers */
+/* 
+window.addEventListener("click", () => {
+    console.log("You knocked?");
+});
+ */
+
+
+/* Events And DOM Nodes */
+/* 
+let button1 = document.querySelector("button");
+button1.addEventListener("click", () => {
+    console.log("Button clicked.");
+});
+ */
+// A node can only have 1 onclick attribute but one can use addEventListener to add multiple handlers
+
+// Using removeEventListener - same named function must be given to both addEventListener and removeEventListener
+/* 
+let button2 = document.querySelectorAll("button")[1];
+function once() {
+    console.log("Done");
+    button2.removeEventListener("click", once);
+}
+button2.addEventListener("click", once);
+ */
+
+/* Event Objects */
+// Event objects are what are passed as arguments to event handlers.
+// let button3 = document.querySelector("button");
+/* 
+let button3 = document.querySelectorAll("button")[2];
+button3.addEventListener("mousedown", event => {
+    if (event.button === 0) {
+        console.log("Left button");
+    } else if (event.button === 1) {
+        console.log("Middle button");
+    } else if (event.button === 2) {
+        console.log("Right button");
+    }
+});
+*/
+
+/* Propagation */
+
+// Stopping event propagation
+// let para = document.querySelector("p");
+let para = document.querySelectorAll("p")[2];
+let button4 = document.querySelectorAll("button")[3];
+para.addEventListener("mousedown", () => {
+    console.log("Handler for paragraph.");
+});
+button4.addEventListener("mousedown", event => {
+    console.log("Handler for button.");
+    if (event.button === 2) {
+        event.stopPropagation();
+    }
+});
