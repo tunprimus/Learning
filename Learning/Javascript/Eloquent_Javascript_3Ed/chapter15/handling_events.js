060fcb284f36845eb41336031cfff165046ba9d2
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-24 22:30:40 +0100

    modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.css 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.html 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js
@@ -117,6 +117,7 @@ window.addEventListener("click", event => {
 /* Mouse Motion */
 // Using mouse motion
 // Tracks the last observed mouse X position
+/* 
 let lastX;
 let bar = document.querySelector("div");
 
@@ -139,4 +140,23 @@ function moved(event) {
         lastX = event.clientX;
     }
 };
+ */
+
+
+/* Touch Events */
+function update(event) {
+    for (let dot; dot = document.querySelector("dot");) {
+        dot.remove();
+    }
+    for (let i = 0; i < event.touches.length; i++) {
+        let {pageX, pageY} = event.touches[i];
+        let dot = document.createElement("dot");
+        dot.style.left = (pageX - 50) + "px";
+        dot.style.top = (pageY - 50) + "px";
+        document.body.appendChild(dot);
+    }
+    window.addEventListener("touchstart", update);
+    window.addEventListener("touchmove", update);
+    window.addEventListener("touchend", update);
+}
 
