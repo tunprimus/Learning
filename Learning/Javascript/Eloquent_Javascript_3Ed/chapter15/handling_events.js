6fa0da7eae04e2c65af8aecdf2ca2c2017399eb9
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-25 10:42:12 +0100

    modified:   Learning/Javascript/Eloquent_Javascript_3Ed/.vscode/settings.json 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter15/handling_events.js
@@ -196,10 +196,15 @@ for (let field of Array.from(fields)) {
 
 /* Events And The Event Loop */
 // Using web workers
+/* 
 let squareWorker = new Worker("code/squareworker.js");
 squareWorker.addEventListener("message", event => {
     console.log("The worker responded:", event.data);
 });
 squareWorker.postMessage(10);
 squareWorker.postMessage(24);
+ */
+
+
+/* Timers */
 
