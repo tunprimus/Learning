19b1716be758e0571ca8cbca074a0a8f51a35230
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-30 00:34:48 +0100

    modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/platform_game.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/platform_game.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/platform_game.js
@@ -284,3 +284,8 @@ State.prototype.update = function (time, keys) {
     }
     return newState;
 };
+
+// Detects if two actors touch
+function overlap(actor1, actor2) {
+    return actor1.pos.x + actor1.size.x > actor2.pos.x && actor1.pos.x < actor2.pos.x + actor2.size.x && actor1.pos.y + actor1.size.y > actor2.pos.y && actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
