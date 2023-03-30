bc88d2fd0dfe841435d27846f0facac14803683f
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-03-30 17:50:46 +0100

    Completed examples in Chapter 16 of book 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/platform_game.html 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/platform_game.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/platform_game.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/platform_game.js
@@ -289,3 +289,146 @@ State.prototype.update = function (time, keys) {
 function overlap(actor1, actor2) {
     return actor1.pos.x + actor1.size.x > actor2.pos.x && actor1.pos.x < actor2.pos.x + actor2.size.x && actor1.pos.y + actor1.size.y > actor2.pos.y && actor1.pos.y < actor2.pos.y + actor2.size.y;
 }
+
+// Mark game as lost if collision with lava occurs
+Lava.prototype.collide = function(state) {
+    return new State(state.level, state.actors, "lost");
+};
+
+// Vanish coins when touched
+Coin.prototype.collide = function(state) {
+    let filtered = state.actors.filter(a => a != this);
+    let status = state.status;
+    if (!filtered.some(a => a.type === "coin")) {
+        status = "won";
+    }
+    return new State(state.level, filtered, status);
+};
+
+// Updating the interaction between lava and actors
+Lava.prototype.update = function(time, state) {
+    let newPos = this.pos.plus(this.speed.times(time));
+    if (!state.level.touches(newPos, this.size, "wall")) {
+        return new Lava(newPos, this.speed, this.reset);
+    } else if (this.reset) {
+        return new Lava(this.reset, this.speed, this.reset);
+    } else {
+        return new Lava(this.pos, this.speed.times(-1));
+    }
+};
+
+// Updating the interaction between coin and actors
+const wobbleSpeed = 8;
+const wobbleDist = 0.07;
+
+Coin.prototype.update = function(time) {
+    let wobble = this.wobble + time * wobbleSpeed;
+    let wobblePos = Math.sin(wobble) * wobbleDist;
+    return new Coin(this.basePos.plus(new Vec(0, wobblePos)), this.basePos, wobble);
+};
+
+// Handling player motion
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
+Player.prototype.update = function(time, state, keys) {
+    // Horizontal speed handling
+    let xSpeed = 0;
+    if (keys.ArrowLeft) {
+        xSpeed -= playerXSpeed;
+    }
+    if (keys.ArrowRight) {
+        xSpeed += playerXSpeed;
+    }
+    let pos = this.pos;
+    let movedX = pos.plus(new Vec(xSpeed * time, 0));
+    if (!state.level,touches(movedX, this.size, "wall")) {
+        pos = movedX;
+    }
+
+    // Vertical speed handling
+    let ySpeed = this.speed.y + time * gravity;
+    let movedY = pos.plus(new Vec(0, ySpeed * time));
+    if (!state.level,touches(movedY, this.size, "wall")) {
+        pos = movedY;
+    } else if (keys.ArrowUp && ySpeed > 0) {
+        ySpeed -= jumpSpeed;
+    } else {
+        ySpeed = 0;
+    }
+    return new Player(pos, new Vec(xSpeed, ySpeed));
+};
+
+/* Tracking Keys */
+
+// Keeps track of keys states and events
+function trackKeys(keys) {
+    let down = Object.create(null);
+    function track(event) {
+        if (keys.includes(event.key)) {
+            down[event.key] = event.type === "keydown";
+            event.preventDefault();
+        }
+    }
+    window.addEventListener("keydown", track);
+    window.addEventListener("keyup", track);
+    return down;
+}
+const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+
+/* Running The Game */
+
+// Helper function for requestAnimationFrame
+function runAnimation(frameFunc) {
+    let lastTime = null;
+    function frame(time) {
+        if (lastTime != null) {
+            let timeStep = Math.min(time - lastTime, 100) / 1000;
+            if (frameFunc(timeStep) === false) {
+                return;
+            }
+        }
+        lastTime = time;
+        requestAnimationFrame(frame);
+    }
+    removeEventListener(frame);
+}
+
+// Takes a Level object and a display constructor to return a promise
+function runLevel(level, Display) {
+    let display = new Display(document.body, level);
+    let state = State.start(level);
+    let ending = 1;
+    return new Promise(resolve => {
+        runAnimation(time => {
+            state = state.update(time, arrowKeys);
+            display.syncState(state);
+            if (state.status === "playing") {
+                return true;
+            } else if (ending > 0) {
+                ending -= time;
+                return true;
+            } else {
+                display.clear();
+                resolve(state.status);
+                return false;
+            }
+        });
+    });
+}
+
+// Handling the game levels
+async function runGame(plans, Display) {
+    for (let level = 0; level < plans.length;) {
+        let status = await runLevel(new Level(plans[level]), Display);
+        if (status === "won") {
+            level++;
+        }
+    }
+    console.log("You have won!");
+}
+
+runGame(GAME_LEVELS, DOMDisplay);
+
