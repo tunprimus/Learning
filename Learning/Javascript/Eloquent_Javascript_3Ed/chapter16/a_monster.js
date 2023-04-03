9b0735b20cd883585aff9ccb9d754cb6f819941a
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-04 00:57:57 +0100

    Partial solution "a monster" exercise in Ch. 16 	new file:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/a_monster.js

---
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter16/a_monster.js
@@ -0,0 +1,29 @@
+/* It is traditional for platform games to have enemies that you can jump on top of to defeat. This exercise asks you to add such an actor type to the game.
+We’ll call it a monster. Monsters move only horizontally. You can make them move in the direction of the player, bounce back and forth like horizontal lava, or have any movement pattern you want. The class doesn’t have to handle falling, but it should make sure the monster doesn’t walk through walls.
+When a monster touches the player, the effect depends on whether the player is jumping on top of them or not. You can approximate this by checking whether the player’s bottom is near the monster’s top. If this is the case, the monster disappears. If not, the game is lost.
+*/
+
+class Monster {
+    constructor(pos, speed, reset) {
+        this.pos = pos;
+        this.speed = speed;
+        this.reset = reset;
+    }
+
+    get type() {
+        return "monster";
+    }
+
+    static create(pos) {
+        return new Monster(pos.plus(new Vec(0, -1)));
+    }
+
+    update(time, state) {}
+
+    collide(state) {}
+}
+
+Monster.prototype.size = new Vec(1.2, 2);
+
+levelChars["M"] = Monster;
+
