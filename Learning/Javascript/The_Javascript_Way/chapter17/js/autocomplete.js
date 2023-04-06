8b33c23f8175a08bf24434e1e961b0fc60d8d54c
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-06 14:02:38 +0100

    Partial solution "autocomplete" exercise Ch. 17 	new file:   Learning/Javascript/The_Javascript_Way/chapter17/css/autocomplete.css 	new file:   Learning/Javascript/The_Javascript_Way/chapter17/html/autocomplete.html 	new file:   Learning/Javascript/The_Javascript_Way/chapter17/js/autocomplete.js

---
+++ Learning/Javascript/The_Javascript_Way/chapter17/js/autocomplete.js
@@ -0,0 +1,46 @@
+/* In this exercise, you’ll have to assist the user in selecting a country. As he enters the country name in an input box, the page shows a list of corresponding countries. Clicking on a suggested country replaces the value in the input box.
+*/
+
+const countryList = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua-and-Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan"
+];
+
+// Add class to suggestion
+const suggestionsElement = document.getElementById("suggestions");
+suggestionsElement.classList.add("suggestion");
+console.log(suggestionsElement);
+
+// Create a select element to hold the options and generate click/change events later
+const selectElement = document.createElement("select");
+
+// Create and return an HTML option tag
+const createOptionElement = (value) => {
+    const optElement = document.createElement("option");
+    optElement.textContent = value;
+    optElement.value = value;
+    return optElement;
+};
+
+// Create and append a dummy default value to the select element as its first child
+selectElement.appendChild(createOptionElement(""));
+
+// Populate the select element with the countries
+countryList.forEach(item => {
+    selectElement.appendChild(createOptionElement(item));
+});
+
+// Add to the div element
+suggestionsElement.appendChild(selectElement);
