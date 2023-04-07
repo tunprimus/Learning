b8bf86050552878e69b30ca58ae74aea1cd9835f
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-07 12:09:04 +0100

    Partial solution "chronometer" exercise Ch. 18 	modified:   Learning/Javascript/The_Javascript_Way/chapter18/css/chronometer.css 	modified:   Learning/Javascript/The_Javascript_Way/chapter18/html/chronometer.html 	modified:   Learning/Javascript/The_Javascript_Way/chapter18/js/chronometer.js

--- Learning/Javascript/The_Javascript_Way/chapter18/js/chronometer.js
+++ Learning/Javascript/The_Javascript_Way/chapter18/js/chronometer.js
@@ -1,2 +1,28 @@
 /* Write an interactive web page with a button to start and stop a chronometer counting the number of elapsed seconds.
 */
+
+/*
+- Get all the elements needed
+- Increment the text displayed
+- Start increments the text
+- Pause allows resume
+- Stop stops the counting with no further resume
+- Reset changes the counter to 0
+*/
+
+const counterElement = document.getElementById("counter");
+const startElement = document.getElementById("start");
+const pauseElement = document.getElementById("pause");
+const stopElement = document.getElementById("stop");
+const resetElement = document.getElementById("reset");
+
+// Counter function
+const increaseCounter = () => {
+    const counter = Number(counterElement.textContent);
+    while (counter < 200) {
+        counterElement.textContent = counter + 1;
+        counter++;
+    }
+};
+
+const validInterval = setInterval(increaseCounter, 1000);
