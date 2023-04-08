3f8904922f03e1019c9c00b1124a1dede43b8fc1
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-08 12:22:58 +0100

    Completed solution "chronometer" exercise Ch. 18 	modified:   Learning/Javascript/The_Javascript_Way/chapter18/html/chronometer.html 	modified:   Learning/Javascript/The_Javascript_Way/chapter18/js/chronometer.js

--- Learning/Javascript/The_Javascript_Way/chapter18/js/chronometer.js
+++ Learning/Javascript/The_Javascript_Way/chapter18/js/chronometer.js
@@ -10,3 +10,44 @@
 - Reset changes the counter to 0
 */
 
+const counterElement = document.getElementById("counter");
+
+const startButtonElement = document.getElementById("start");
+const stopButtonElement = document.getElementById("stop");
+const resetButtonElement = document.getElementById("reset");
+
+let count = 0;
+let timerStatus = 0;
+let timer = null;
+
+function startTimer() {
+    // First call to the timer
+    if (timerStatus === 0) {
+        timer = setInterval(startTimer, 1000);
+        timerStatus = 2;
+    }
+    count++;
+    counterElement.innerHTML = count;
+
+    // When stop and reset are active
+    if (timerStatus === 1) {
+        timer = setInterval(startTimer, 1000);
+        timerStatus = 2;
+    }
+}
+
+function stopTimer() {
+    timerStatus = 1;
+    clearInterval(timer);
+}
+
+function resetTimer() {
+    count = 0;
+    timerStatus = 1;
+    clearInterval(timer);
+    counterElement.innerHTML = count;
+}
+
+startButtonElement.addEventListener("click", startTimer);
+stopButtonElement.addEventListener("click", stopTimer);
+resetButtonElement.addEventListener("click", resetTimer);
