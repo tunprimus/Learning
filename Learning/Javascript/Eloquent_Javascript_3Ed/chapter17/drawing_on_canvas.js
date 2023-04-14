ec67b1899da9c091967a2819801e0e450d76b2ad
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-14 08:36:58 +0100

    modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter17/drawing_on_canvas.html 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter17/drawing_on_canvas.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter17/drawing_on_canvas.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter17/drawing_on_canvas.js
@@ -35,3 +35,35 @@ cx3.lineTo(10, 70);
 cx3.lineTo(90,70);
 cx3.fill();
 
+/* Curves */
+
+let cx4 = document.querySelectorAll("canvas")[4].getContext("2d");
+cx4.beginPath();
+cx4.moveTo(10, 90);
+// control = (60, 10) goal = (90, 90)
+cx4.quadraticCurveTo(60, 10, 90, 90);
+cx4.lineTo(60, 10);
+cx4.closePath();
+cx4.stroke();
+
+let cx5 = document.querySelectorAll("canvas")[5].getContext("2d");
+cx5.beginPath();
+cx5.moveTo(10, 90);
+// control1 = (10, 10) control2 = (90, 10) goal = (50, 90)
+cx5.bezierCurveTo(10, 10, 90, 10, 50, 90);
+cx5.lineTo(90, 10);
+cx5.lineTo(10, 10);
+cx5.closePath();
+cx5.stroke();
+
+
+let cx6 = document.querySelectorAll("canvas")[6].getContext("2d");
+cx6.beginPath();
+// centre = (50, 50) radius = 40 angle = 0 to 7
+cx6.arc(50, 50, 40, 0, 7);
+// centre = (150, 50) radius = 40 angle = 0 to 1/2PI
+cx6.arc(150, 50, 40, 0, 0.5 * Math.PI);
+cx6.stroke();
+
+
+/* Drawing A Pie Chart */
