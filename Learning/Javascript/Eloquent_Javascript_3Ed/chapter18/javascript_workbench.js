6b7f87b6a4a340ca794f767adee48b56707ea701
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-25 00:40:59 +0100

    Partial solution to "a javascript workbench" exercise in Ch.18 	new file:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/javascript_workbench.html 	new file:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/javascript_workbench.js

---
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/javascript_workbench.js
@@ -0,0 +1,5 @@
+/* Build an interface that allows people to type and run pieces of JavaScript code.
+Put a button next to a <textarea> field that, when pressed, uses the Function constructor we saw in Chapter 10 to wrap the text in a function and call it. Convert the return value of the function, or any error it raises, to a string and display it below the text field.
+*/
+
+
