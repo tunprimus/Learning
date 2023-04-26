764b4c4d69dc2a06111b25c3b9029fb8402e7f90
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-26 09:57:47 +0100

    Partial solution "javascript workbench" exercise in Ch. 18 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/javascript_workbench.html 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/javascript_workbench.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/javascript_workbench.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/javascript_workbench.js
@@ -2,4 +2,7 @@
 Put a button next to a <textarea> field that, when pressed, uses the Function constructor we saw in Chapter 10 to wrap the text in a function and call it. Convert the return value of the function, or any error it raises, to a string and display it below the text field.
 */
 
-
+const formElement = document.querySelector("form");
+const textareaElement = document.querySelector("textarea");
+const buttonElement = document.querySelector("button");
+const spanElement = document.querySelector("span");
