16b06098606e924aa598b18aa6944cff3591f7f7
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-26 22:07:36 +0100

    Partial solution "Conway's game of life" exercise in Ch. 18 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/conways_game_of_life.html 	modified:   Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/conways_game_of_life.js

--- Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/conways_game_of_life.js
+++ Learning/Javascript/Eloquent_Javascript_3Ed/chapter18/conways_game_of_life.js
@@ -11,11 +11,62 @@ initially. Display it as a grid of checkbox fields, with a button next to it to 
 /* Probable Steps
 - Create button to run simulation
 - Create button to stop simulation
-- Define minimum cell number constant
-- Define maximum cell number constant
+- Define minimum cell alive constant
+- Define maximum cell alive constant
 - May use array, object or Map data structure
 - Generate grid
 - Generate checkboxes of inputs and append to grid
 - Randomly check some of the inputs at outset
 - Check state of the game regularly
 */
+
+
+/* Variables and constants */
+const GRID_WIDTH = 8;
+const GRID_HEIGHT = 8;
+const GRID_AREA = GRID_WIDTH * GRID_HEIGHT;
+const MIN_NEIGHBOURS_ALIVE = 2;
+const MAX_NEIGHBOURS_ALIVE = 3;
+
+const gridElement = document.getElementById("grid");
+const nextElement = document.getElementById("next");
+const runElement = document.getElementById("run");
+const stopElement = document.getElementById("stop");
+
+/* Grid Generation */
+
+const checkboxes = [];
+for (let y = 0; y < GRID_HEIGHT; y++) {
+    for (let x = 0; x < GRID_WIDTH; x++) {
+        let cell = document.createElement("input");
+        cell.type = "checkbox";
+        gridElement.appendChild(cell);
+        checkboxes.push(cell);
+    }
+    // Need to split the  arrays at the end of each row
+    gridElement.appendChild(document.createElement("br"));
+}
+
+/* Handling The Simulation */
+
+let simulate = null;
+
+function runSimulation() {
+    simulate = setInterval(() => {
+        // Function to update the grid
+    }, 1000);
+    runElement.disabled = true;
+    nextElement.disabled = true;
+    stopElement.style.display = "inline-block";
+}
+
+function stopSimulation() {
+    clearInterval(simulate);
+    runElement.disabled = false;
+    nextElement.disabled = false;
+    stopElement.style.display = "none";
+}
+
+runElement.addEventListener("click", runSimulation);
+
+stopElement.addEventListener("click", stopSimulation);
