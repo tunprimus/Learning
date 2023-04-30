30ba2e19121e9042d72943212bd85c3476d82209
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-04-30 08:56:32 +0100

    Starting solution "star wars universe" exercise in Ch. 22 	modified:   Learning/Javascript/The_Javascript_Way/.vscode/settings.json 	new file:   Learning/Javascript/The_Javascript_Way/chapter22/css/star_wars.css 	new file:   Learning/Javascript/The_Javascript_Way/chapter22/html/star_wars.html 	new file:   Learning/Javascript/The_Javascript_Way/chapter22/js/star_wars.js

---
+++ Learning/Javascript/The_Javascript_Way/chapter22/js/star_wars.js
@@ -0,0 +1,13 @@
+/* The open Star Wars API9 has all the Star Wars data you’ve ever wanted. In this exercise, you’ll show information about some of the planets in the universe.
+Write the associated JavaScript code so that a list of links for the first ten planets identifiers (from 1 to 10) is generated automatically. Clicking on a planet link shows information about it.
+*/
+
+const planetLinksElement = document.getElementById("links");
+const planetinfosElement = document.getElementById("infos");
+
+// Function to show information about a planet using the API
+
+
+
+
+// Function to generate links to display planetary info
