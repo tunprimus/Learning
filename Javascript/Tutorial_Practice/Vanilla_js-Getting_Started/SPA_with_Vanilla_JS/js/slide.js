9f00f6b85ee08a7332308baa085cafe29ca129c4
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-05-27 02:11:50 +0100

    new file:   Javascript/Tutorial_Practice/Vanilla_js-Getting_Started/SPA_with_Vanilla_JS/js/slide.js

---
+++ Javascript/Tutorial_Practice/Vanilla_js-Getting_Started/SPA_with_Vanilla_JS/js/slide.js
@@ -0,0 +1,101 @@
+
+
+import { DataBinding } from "./dataBinding.js";
+
+/** 
+ * Represents a slide 
+ * */
+export class Slide {
+    /**
+     * @constructor
+     * @param {string} text - The content of the slide 
+     */
+    constructor(text) {
+        /** 
+         * Internal text representation of the slide
+         * @type {string}
+         */
+        this._text = text;
+        /**
+         * Context for embedded scripts
+         * @type {object}
+         */
+        this._context = { };
+        /**
+         * Data binding helper
+         * @type {DataBinding}
+         */
+        this._dataBinding = new DataBinding();
+        /** 
+         * The HTML DOM hosting the slide contents
+         * @type {HTMLDivElement}
+         */
+        this._html = document.createElement("div");
+        this._html.innerHTML = text;
+        /**
+         * The title of the slide
+         * @type {string}
+         */
+        this._title = this._html.querySelectorAll("title")[0].innerText;
+        /** @type{NodeListOf<HTMLElement>} */
+        const transition = (this._html.querySelectorAll("transition"));
+        if (transition.length) {
+            /**
+             * The name of the animation to use for transitions
+             * @type {string}
+             */
+            this._transition = transition[0].innerText;
+        } else {
+            this._transition = null;
+        }
+        /** @type{NodeListOf<HTMLElement>} */
+        const hashNext = this._html.querySelectorAll("next-slide");
+        if (hashNext.length > 0) {
+            /** 
+             * The name of the next slide to load
+             * @type {string}
+             */
+            this._nextSlideName = hashNext[0].innerText;
+        } else {
+            this._nextSlideName = null;
+        }
+        // execute any scripts
+        const script = this._html.querySelector("script");
+        if (script) {
+            this._dataBinding.executeInContext(script.innerText, this._context, true);
+            this._dataBinding.bindAll(this._html, this._context);
+        }
+    }
+
+    /** 
+     * The slide transition
+     * @returns {string} The transition name
+     */
+    get transition() {
+        return this._transition;
+    }
+
+    /** 
+     * The slide title
+     * @returns {string} The slide title
+     */
+    get title() {
+        return this._title;
+    }
+
+    /**
+     * The HTML DOM node for the slide
+     * @returns {HTMLDivElement} The HTML content
+     */
+    get html() {
+        return this._html;
+    }
+
+    /**
+     * The name of the next slide (filename without the .html extension)
+     * @returns {string} The name of the next slide
+     */
+    get nextSlide() {
+        return this._nextSlideName;
+    }
+}
