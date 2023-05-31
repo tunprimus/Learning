a17ffa2f6e954aab5b1e0df4d9ca8becd51169dd
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-05-31 17:00:46 +0100

    modified:   Javascript/Tutorial_Practice/Modern-TodoMVC-VanillaJS/js/helpers.js 	modified:   Javascript/Tutorial_Practice/Modern-TodoMVC-VanillaJS/js/store.js

--- Javascript/Tutorial_Practice/Modern-TodoMVC-VanillaJS/js/helpers.js
+++ Javascript/Tutorial_Practice/Modern-TodoMVC-VanillaJS/js/helpers.js
@@ -1,2 +1,19 @@
 /* TodoMVC App Written in Vanilla JS in 2022
     https://github.com/1Marc/modern-todomvc-vanillajs */
+
+export const getURLHash = () => document.location.hash.replace(/^#\//, "");
+
+export const delegate = (elem, selector, event, handler) => {
+    elem.addEventListener(event, (e) => {
+        if (e.target.matches(selector)) {
+            handler(e, elem);
+        }
+    });
+};
+
+export const insertHTML = (elem, html) => elem.insertAdjacentHTML("afterbegin", html);
+
+export const replaceHTML = (elem, html) => {
+    elem.replaceChildren();
+    insertHTML(elem, html);
+};
--- Javascript/Tutorial_Practice/Modern-TodoMVC-VanillaJS/js/store.js
+++ Javascript/Tutorial_Practice/Modern-TodoMVC-VanillaJS/js/store.js
@@ -1,2 +1,19 @@
 /* TodoMVC App Written in Vanilla JS in 2022
     https://github.com/1Marc/modern-todomvc-vanillajs */
+
+export const TodoStore = class extends EventTarget {
+    constructor(localStorageKey) {
+        super();
+        this.localStorageKey = localStorageKey;
+        this.readStorage();
+        // handle todos edited in another window
+        window.addEventListener("storage", () => {
+            this._readStorage();
+            this._save();
+        }, false);
+        // GETTER methods
+        this.get = (id) => this.todos.find((todo) => todo.id === id);
+        this.isAllCompleted = () => this.todos.every((todo) => todo.completed);
+        this.hasCompleted = () => this.todos.some((todo) => todo.completed);
+    }
+};
