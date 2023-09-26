54b1c6f818b50ac2332b5bc6ca345591f703e953
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-09-26 06:40:39 +0100

    feat: Add habits adding and tracking functionality

--- Javascript/Tutorial_Practice/Habit-Tracking-App/main.js
+++ Javascript/Tutorial_Practice/Habit-Tracking-App/main.js
@@ -2,6 +2,7 @@
 
 // Query Selectors
 const habits = document.querySelectorAll('.habit-btn');
+const habitContainer = document.querySelector('.habit-container');
 const themeBtn = document.querySelector('#theme');
 const modalContainer = document.querySelector('.modal-container');
 const createHabitBtn = document.querySelector('.new-habit__add');
@@ -18,6 +19,35 @@ const storage = {
   },
   checkTheme() {
     return localStorage.getItem('habitsapp.theme');
+  },
+  saveHabit(obj) {
+    const currentHabits = storage.getHabits();
+    if (currentHabits === null || currentHabits === '') {
+      localStorage.setItem('habitsapp.habits', JSON.stringify(obj));
+    } else {
+      currentHabits.push(obj);
+      localStorage.setItem('habitsapp.habits', JSON.stringify(currentHabits));
+    }
+    // console.table(currentHabits);
+  },
+  getHabits() {
+    let currentHabits;
+    if (localStorage.getItem('habitsapp.habits') === null) {
+      currentHabits = [];
+    } else {
+      currentHabits = JSON.parse(localStorage.getItem('habitsapp.habits'));
+    }
+    return currentHabits;
+  },
+  habitStatus(id) {
+    const currentHabits = storage.getHabits();
+    currentHabits.forEach(habit => {
+      if (habit.id !== Number(id)) {
+        return;
+      }
+      habit.completed === true ? habit.completed = false : habit.completed = true;
+    });
+    localStorage.setItem('habitsapp.habits', JSON.stringify(currentHabits));
   },
 };
 
@@ -44,6 +74,28 @@ const ui = {
       icon.classList.remove('selected');
     });
   },
+  addNewHabit(title, icon, id, completed) {
+    const habitDiv = document.createElement('div');
+    habitDiv.classList.add('habit');
+    habitDiv.innerHTML = `
+      <button class="habit-btn ${completed === true ? 'completed' : ''}" data-id="${id}" data-title="${title}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          ${icon}
+        </svg>
+      </button>
+    `;
+    habitContainer.appendChild(habitDiv);
+  },
+  refreshHabits() {
+    const uiHabits = document.querySelectorAll('.habit');
+    uiHabits.forEach(habit => habit.remove());
+    const currentHabits = storage.getHabits();
+
+    currentHabits.forEach(habit => {
+      ui.addNewHabit(habit.title, habit.icon, habit.id, habit.completed);
+    });
+    console.table(currentHabits);
+  },
 };
 
 // EVENT LISTENERS
@@ -55,6 +107,8 @@ window.addEventListener('DOMContentLoaded', () => {
   if (theme === 'dark') {
     ui.theme();
   }
+  // Update UI
+  ui.refreshHabits();
 });
 
 // EVENT: theme button
@@ -74,10 +128,44 @@ icons.forEach(icon => {
   });
 });
 
-// DELETE
+// EVENT: add new habit button
+addBtn.addEventListener('click', () => {
+  const habitTitle = newHabitTitle.value;
+  let habitIcon;
+  icons.forEach(icon => {
+    if (!icon.classList.contains('selected')) {
+      return;
+    }
+    habitIcon = icon.querySelector('svg').innerHTML;
+  });
+  const habitID = Math.random();
+  ui.addNewHabit(habitTitle, habitIcon, habitID);
+  ui.closeModal();
+  const habit = {
+    title: habitTitle,
+    icon: habitIcon,
+    id: habitID,
+    completed: false,
+  };
+  storage.saveHabit(habit);
+});
 
+
+
+
+// EVENT: complete a habit
+habitContainer.addEventListener('click', evt => {
+  if (!evt.target.classList.contains('habit-btn')) {
+    return;
+  }
+  // ui.habitChecker(evt.target);
+  evt.target.classList.toggle('completed');
+  storage.habitStatus(evt.target.dataset.id);
+});
+/* 
 habits.forEach(habit => {
   habit.addEventListener('click', () => {
     habit.classList.toggle('completed');
   });
 });
+ */
