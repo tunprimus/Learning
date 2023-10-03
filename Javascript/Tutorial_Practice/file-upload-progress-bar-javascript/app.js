bacd7388a81c8003d9ce5269147e647635cb9d6c
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-10-03 22:03:46 +0100

    Functional code "Upload File with Vanilla JavaScript and Loading Animation"

---
+++ Javascript/Tutorial_Practice/file-upload-progress-bar-javascript/app.js
@@ -0,0 +1,70 @@
+/* Upload File with Vanilla JavaScript and Loading Animation #+++>>> https://stackabuse.com/upload-file-with-vanilla-javascript-and-loading-animation/ */
+
+const uploadForm = document.querySelector('.file-upload__form');
+const myInput = document.querySelector('.file-input');
+const progressContainer = document.querySelector('.progress-container');
+const uploadedContainer = document.querySelector('.uploaded-container');
+
+uploadForm.addEventListener('click', () => {
+  myInput.click();
+});
+
+myInput.onchange = ({ target }) => {
+  let file = target.files[0];
+  if (file) {
+    let fileName = file.name;
+    if (fileName.length >= 12) {
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + '... .' + splitName[1];
+    }
+    uploadFile(fileName);
+  }
+};
+
+function uploadFile(name) {
+  let xhrRequest = new XMLHttpRequest();
+  const endpoint = 'uploadFile.php';
+  xhrRequest.open('POST', endpoint);
+  xhrRequest.upload.addEventListener('progress', ({ loaded, total }) => {
+    let fileLoaded = Math.floor((loaded / total) * 100);
+    let fileTotal = Math.floor(total / 1000);
+    let fileSize;
+    fileTotal < 1024 
+    ? (fileSize = fileTotal + ' KB') 
+    : (fileSize = (loaded / (1024 * 1024).toFixed(2) + ' MB'));
+    let progressMarkup = `<li class="row section__row">
+        <i class="fas fa-file-alt section__icon"></i>
+        <div class="content-wrapper">
+          <div class="details-wrapper">
+            <span class="name">${name} | <span>Uploading</span></span>
+            <span class="percent">${fileLoaded}%</span>
+          </div>
+          <div class="progress-bar-wrapper">
+            <div class="progress-wrapper" style="width: ${fileLoaded}%"></div>
+          </div>
+        </div>
+      </li>`;
+    
+    uploadedContainer.classList.add('onprogress');
+    progressContainer.innerHTML = progressMarkup;
+
+    if (loaded === total) {
+      progressContainer.innerHTML = '';
+      let uploadedMarkup = `<li class="row">
+          <div class="content-wrapper upload">
+            <i class="fas fa-file-alt"></i>
+            <div class="details-wrapper">
+              <span class="name">${name} | <span>Uploaded</span></span>
+              <span class="file-size">${fileSize}</span>
+            </div>
+          </div>
+        </li>`;
+      
+      uploadedContainer.classList.remove("onprogress");
+      uploadedContainer.insertAdjacentHTML("afterbegin", uploadedMarkup);
+    }
+  });
+  
+  let data = new FormData(uploadForm);
+  xhrRequest.send(data);
+}
