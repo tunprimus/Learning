35b1b4e8ae627ec34947b65a141de1573e270ce4
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-10-14 04:14:47 +0100

    fix: Correct left arrow not moving to last slide

---
+++ Javascript/Tutorial_Practice/3-JavaScript-Projects-Every-Beginner-Should-Build/slider.js
@@ -0,0 +1,41 @@
+const leftElem = document.querySelector('.left');
+const rightElem = document.querySelector('.right');
+const sliderElem = document.querySelector('.slider');
+const imagesElem = document.querySelectorAll('.image');
+
+let slideNumber = 1;
+const slideLength = imagesElem.length;
+
+const nextSlide = () => {
+  sliderElem.style.transform = `translateX(-${slideNumber * 100}%)`;
+    slideNumber++;
+};
+
+const prevSlide = () => {
+  sliderElem.style.transform = `translateX(-${(slideNumber - 2) * 100}%)`;
+    slideNumber--;
+};
+
+const getFirstSlide = () => {
+  sliderElem.style.transform = `translateX(-${slideNumber * 0}%)`;
+  slideNumber = 1;
+};
+
+const getLastSlide = () => {
+  sliderElem.style.transform = `translateX(-${(slideNumber - 1) * 100}%)`;
+  slideNumber = slideLength;
+};
+
+const jumpToEndSlide = () => {
+  sliderElem.style.transform = `translateX(-${(slideLength - 1) * 100}%)`;
+  slideNumber = slideLength;
+};
+
+rightElem.addEventListener('click', () => {
+  slideNumber < slideLength ? nextSlide() : getFirstSlide();
+});
+
+leftElem.addEventListener('click', () => {
+  slideNumber === 1 ? jumpToEndSlide() 
+    : slideNumber > 1 ? prevSlide() : getLastSlide();
+});
