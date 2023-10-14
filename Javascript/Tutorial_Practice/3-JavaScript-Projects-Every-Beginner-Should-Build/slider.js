1010bdd0d1efba5f97dc5a938957bb8793352f4d
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-10-14 10:18:58 +0100

    refactor: Change position button functionality

--- Javascript/Tutorial_Practice/3-JavaScript-Projects-Every-Beginner-Should-Build/slider.js
+++ Javascript/Tutorial_Practice/3-JavaScript-Projects-Every-Beginner-Should-Build/slider.js
@@ -10,6 +10,33 @@ const buttonsElem = document.querySelectorAll('.button');
 let slideNumber = 1;
 const slideLength = imagesElem.length;
 
+if (slideLength > DEFAULT_BUTTONS_NUMBER) {
+  const fragment = new DocumentFragment();
+  for (let i = DEFAULT_BUTTONS_NUMBER; i < slideLength; i++) {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'button';
+    fragment.appendChild(button);
+  }
+  bottomElem.appendChild(fragment);
+}
+
+buttonsElem[0].style.backgroundColor = 'white';
+
+const resetBtnBg = () => {
+  buttonsElem.forEach(btn => {
+    btn.style.backgroundColor = 'transparent';
+  });
+};
+
+buttonsElem.forEach((button, pos) => {
+  button.addEventListener('click', () => {
+    resetBtnBg();
+    sliderElem.style.transform = `translateX(-${pos * 100}%)`;
+    button.style.backgroundColor = 'white';
+  });
+});
+
 const nextSlide = () => {
   sliderElem.style.transform = `translateX(-${slideNumber * 100}%)`;
     slideNumber++;
@@ -44,29 +71,4 @@ leftElem.addEventListener('click', () => {
     : slideNumber > 1 ? prevSlide() : getLastSlide();
 });
 
-if (slideLength > DEFAULT_BUTTONS_NUMBER) {
-  const fragment = new DocumentFragment();
-  for (let i = DEFAULT_BUTTONS_NUMBER; i < slideLength; i++) {
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.className = 'button';
-    fragment.appendChild(button);
-  }
-  bottomElem.appendChild(fragment);
-}
 
-buttonsElem[0].style.backgroundColor = 'white';
-
-const resetBtnBg = () => {
-  buttonsElem.forEach(btn => {
-    btn.style.backgroundColor = 'transparent';
-  });
-};
-
-buttonsElem.forEach((button, pos) => {
-  button.addEventListener('click', () => {
-    resetBtnBg();
-    sliderElem.style.transform = `translateX(-${pos * 100}%)`;
-    button.style.backgroundColor = 'white';
-  });
-});
