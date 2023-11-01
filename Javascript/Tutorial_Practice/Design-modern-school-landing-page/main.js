149ce6f061a5cd9d38ee9a598de21d467a21b292
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-11-01 16:03:59 +0100

    feat: Navigation codes

--- Javascript/Tutorial_Practice/Design-modern-school-landing-page/main.js
+++ Javascript/Tutorial_Practice/Design-modern-school-landing-page/main.js
@@ -1,1 +1,13 @@
 /* Design a Modern School Landing Page | HTML, CSS & Vanilla JS Tutorial by Mute Coder | https://www.youtube.com/watch?v=83OO97Zy8Dw */
+
+const toggleMenuElement = document.querySelector('#toggle-menu');
+const navigationElement = document.querySelector('#navigation');
+
+/**
+ * Navigation
+ */
+
+function toggleMenu() {
+  toggleMenuElement.classList.toggle('active');
+  navigationElement.classList.toggle('active');
+}
