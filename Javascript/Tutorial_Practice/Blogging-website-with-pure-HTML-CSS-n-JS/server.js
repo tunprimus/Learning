fc89fc5d70f87a406750d0790752eb4f7d2afd08
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-11-03 23:39:28 +0100

    feat: Add blog and 404 routes in server.js

--- Javascript/Tutorial_Practice/Blogging-website-with-pure-HTML-CSS-n-JS/server.js
+++ Javascript/Tutorial_Practice/Blogging-website-with-pure-HTML-CSS-n-JS/server.js
@@ -42,3 +42,11 @@ app.post('/upload', (req, res) => {
     }
   });
 });
+
+app.get('/:blog', (req, res) => {
+  res.sendFile(path.join(initial_path, 'blog.html'));
+});
+
+app.use((req, res) => {
+  res.json('404');
+});
