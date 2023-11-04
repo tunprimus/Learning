169e18aabf860bbad3113dd5ec695335bbf7e956
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-11-04 06:18:10 +0100

    feat: Add addArticle function

--- Javascript/Tutorial_Practice/Blogging-website-with-pure-HTML-CSS-n-JS/public/js/blog.js
+++ Javascript/Tutorial_Practice/Blogging-website-with-pure-HTML-CSS-n-JS/public/js/blog.js
@@ -31,3 +31,34 @@ const setupBlog = (data) => {
   const article = document.querySelector('article');
   addArticle(article, data.article);
 };
+
+const addArticle = (elem, data) => {
+  data = data.split('\n').filter(item => item.length);
+
+  data.forEach(item => {
+    if (item[0] === '#') {
+      let hCount = 0;
+      let i = 0;
+      while (item[i] === '#') {
+        hCount++;
+        i++;
+      }
+      let tag = `h${hCount}`;
+      elem.innerHTML += `<${tag}>${item.slice(hCount, item.length)}</${tag}>`;
+    } else if (item[0] === '!' && item[1] === '[') {
+      let separator;
+
+      for (let i = 0; i < item.length; i++) {
+        if (item[i] === ']' && item[i + 1] === '(' && item[item.length - 1] === ')') {
+          separator = i;
+        }
+      }
+
+      let alt = item.slice(2, separator);
+      let src = item.slice(separator + 2, item.length - 1);
+      elem.innerHTML += `<img src="${src}" alt="${alt}" class="article-image">`;
+    } else {
+      elem.innerHTML += `<p>${item}</p>`;
+    }
+  });
+};
