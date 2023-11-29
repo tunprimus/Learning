2a5ba2b840cb11ccda8701a37f46f290d50aa8bb
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-11-29 03:59:21 +0100

    feat: Add pallet colours codes

--- Javascript/Tutorial_Practice/PWA-Bridging-the-Gap-btw-Web-n-Mobile/colourpicker-pwa/data/colour-data.js
+++ Javascript/Tutorial_Practice/PWA-Bridging-the-Gap-btw-Web-n-Mobile/colourpicker-pwa/data/colour-data.js
@@ -1,1 +1,49 @@
 /* PWA: Bridging the Gap Between Web and Mobile Development | https://blog.greenroots.info/how-to-build-a-progressive-web-app-using-javascript-and-vite */
+
+export const colours = [
+  {
+    'data': '#de3516',
+  },
+  {
+    'data': '#ef9baf',
+  },
+  {
+    'data': '#5903b2',
+  },
+  {
+    'data': '#6a8217',
+  },
+  {
+    'data': '#a40115',
+  },
+  {
+    'data': '#fbf9ad',
+  },
+  {
+    'data': '#9966CC',
+  },
+  {
+    'data': '#0AFFFF',
+  },
+  {
+    'data': '#943241',
+  },
+  {
+    'data': '#FFD700',
+  },
+  {
+    'data': '#839496',
+  },
+  {
+    'data': '#FFC87C',
+  },
+  {
+    'data': '#D33682',
+  },
+  {
+    'data': '#002B36',
+  },
+  {
+    'data': '#EAE0C8',
+  },
+];
--- Javascript/Tutorial_Practice/PWA-Bridging-the-Gap-btw-Web-n-Mobile/colourpicker-pwa/main.js
+++ Javascript/Tutorial_Practice/PWA-Bridging-the-Gap-btw-Web-n-Mobile/colourpicker-pwa/main.js
@@ -1,24 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import { colours } from "./data/colour-data.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const createColourPallet = () => {
+  colours.forEach((colour) => {
+    const colourPallet = document.createElement('div');
+    colourPallet.classList.add('colour-pallet');
+    colourPallet.style.backgroundColor = colour.data;
+    const colourName = document.createElement('div');
+    colourName.classList.add('colour-name');
+    colourName.textContent = colour.data;
+    colourPallet.appendChild(colourName);
+    colourPallet.addEventListener('click', () => {
+      const colourCode = colour.data;
+      console.log(colourCode);
+      copyContent(colourCode);
+    });
+    document.querySelector('#app').appendChild(colourPallet);
+  });
+};
 
-setupCounter(document.querySelector('#counter'))
+const copyContent = async (text) => {
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log('Content copied to clipboard');
+    alert(`The colour ${text} has been copied successfully`);
+  } catch (err) {
+    console.error('Failed to copy: ', err);
+  }
+};
+
+createColourPallet();
