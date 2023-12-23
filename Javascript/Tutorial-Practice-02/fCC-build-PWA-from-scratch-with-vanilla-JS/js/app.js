0a01f1bd52a87e0473d7ed2cb1bad36db6f441eb
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-12-23 15:21:05 +0100

    feat: Add service worker registration to app.js

--- Javascript/Tutorial-Practice-02/fCC-build-PWA-from-scratch-with-vanilla-JS/js/app.js
+++ Javascript/Tutorial-Practice-02/fCC-build-PWA-from-scratch-with-vanilla-JS/js/app.js
@@ -28,3 +28,12 @@ function showCoffees() {
 }
 
 document.addEventListener('DOMContentLoaded', showCoffees);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    this.navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then(res => console.log('service worker registered'))
+      .catch(err => console.error('service worker not registered', err))
+  });
+}
