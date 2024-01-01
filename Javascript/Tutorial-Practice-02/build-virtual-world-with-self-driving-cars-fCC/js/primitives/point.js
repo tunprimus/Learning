5d63edb3899161060c2284de92c9941b06e320a6
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-01-01 19:07:45 +0100

    feat: Add classes for graph, point and segment

---
+++ Javascript/Tutorial-Practice-02/build-virtual-world-with-self-driving-cars-fCC/js/primitives/point.js
@@ -0,0 +1,14 @@
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	draw(ctx, size = 18, colour = 'black') {
+		const rad = size / 2;
+		ctx.beginPath();
+		ctx.fillStyle = colour;
+		ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
+		ctx.fill();
+	}
+}
---
+++ Javascript/Tutorial-Practice-02/build-virtual-world-with-self-driving-cars-fCC/js/primitives/segment.js
@@ -0,0 +1,15 @@
+class Segment {
+	constructor(p1, p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+
+	draw(ctx, width = 2, colour = 'black') {
+		ctx.beginPath();
+		ctx.lineWidth = width;
+		ctx.strokeStyle = colour;
+		ctx.moveTo(this.p1.x, this.p1.y);
+		ctx.lineTo(this.p2.x, this.p2.y);
+		ctx.stroke();
+	}
+}
