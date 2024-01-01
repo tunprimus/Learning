e9a775baa628ac0f576ded1baf95ae52c1561cfd
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-01-01 20:00:54 +0100

    feat: Add random point functionality

--- Javascript/Tutorial-Practice-02/build-virtual-world-with-self-driving-cars-fCC/js/primitives/point.js
+++ Javascript/Tutorial-Practice-02/build-virtual-world-with-self-driving-cars-fCC/js/primitives/point.js
@@ -4,6 +4,10 @@ class Point {
 		this.y = y;
 	}
 
+	equals(point) {
+		return this.x === point.x && this.y === point.y;
+	}
+
 	draw(ctx, size = 18, colour = 'black') {
 		const rad = size / 2;
 		ctx.beginPath();
