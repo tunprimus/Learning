25ed3a56fb471deb16cdee3509ebdff18aa40c7b
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-01-05 16:57:11 +0100

    feat: Make keep camera position on car while moving

--- Javascript/Tutorial-Practice-02/self-driving-car-JS-course/main.js
+++ Javascript/Tutorial-Practice-02/self-driving-car-JS-course/main.js
@@ -1,6 +1,7 @@
 
 const canvas = document.getElementById('my-canvas');
 canvas.width = 200;
+const CAR_ON_CANVAS_OFFSET = 0.7;
 
 const ctx = canvas.getContext('2d');
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
@@ -12,7 +13,13 @@ animate();
 function animate() {
 	car.update();
 	canvas.height = window.innerHeight;
+
+	ctx.save();
+	ctx.translate(0, -car.y + canvas.height * CAR_ON_CANVAS_OFFSET);
+
 	road.draw(ctx);
 	car.draw(ctx);
+
+	ctx.restore();
 	requestAnimationFrame(animate);
 }
