dd99e29a9c3264c14809fea1c72b5b4ef5f69483
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-01-05 18:14:17 +0100

    fix: Remove duplicate car.draw(ctx) call in main to restore functionality

--- Javascript/Tutorial-Practice-02/self-driving-car-JS-course/main.js
+++ Javascript/Tutorial-Practice-02/self-driving-car-JS-course/main.js
@@ -6,7 +6,7 @@ const CAR_ON_CANVAS_OFFSET = 0.7;
 const ctx = canvas.getContext('2d');
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCentre(1), 100, 30, 50);
-car.draw(ctx);
+// car.draw(ctx);
 
 animate();
 
