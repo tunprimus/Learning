78f8224b06fb11821fabc170fc7fb10563d97e93
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-01-12 17:07:39 +0100

    fix: Always keep sensor rays align to car

--- Javascript/Tutorial-Practice-02/self-driving-car-JS-course/sensor.js
+++ Javascript/Tutorial-Practice-02/self-driving-car-JS-course/sensor.js
@@ -12,7 +12,7 @@ class Sensor {
 	update() {
 		this.rays = [];
 		for (let i = 0; i < this.rayCount; i++) {
-			const rayAngle = lerp(this.raySpread / 2, -this.raySpread / 2, i / (this.rayCount - 1));
+			const rayAngle = lerp(this.raySpread / 2, -this.raySpread / 2, i / (this.rayCount - 1)) + this.car.angle;
 
 			const start = {x: this.car.x, y: this.car.y};
 			const end = {x: this.car.x - Math.sin(rayAngle) * this.rayLength, y: this.car.y - Math.cos(rayAngle) * this.rayLength};
