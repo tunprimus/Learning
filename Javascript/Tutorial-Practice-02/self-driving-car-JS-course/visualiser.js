7fbe1c0e5b11c2890e3359ce261703c3b484e1a1
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-01-16 13:59:37 +0100

    refactor: Destructure level into inputs and outputs

--- Javascript/Tutorial-Practice-02/self-driving-car-JS-course/visualiser.js
+++ Javascript/Tutorial-Practice-02/self-driving-car-JS-course/visualiser.js
@@ -17,9 +17,11 @@ class Visualiser {
 		const right = left + width;
 		const bottom = top + height;
 
+		const {inputs, outputs} = level;
+
 		const nodeRadius = NODE_RADIUS;
-		for (let i = 0; i < level.inputs.length; i++) {
-			const x = lerp(left, right, level.inputs.length === 1 ? 0.5 : i / (level.inputs.length - 1));
+		for (let i = 0; i < inputs.length; i++) {
+			const x = lerp(left, right, inputs.length === 1 ? 0.5 : i / (inputs.length - 1));
 			ctx.beginPath();
 			ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2);
 			ctx.fillStyle = 'white';
