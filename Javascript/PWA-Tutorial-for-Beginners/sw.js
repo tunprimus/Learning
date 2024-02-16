6b258961884e53fd311859402869edd07838f9e2
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-02-16 18:00:22 +0100

    fix: Add theme colour metadata for PWA

---
+++ Javascript/PWA-Tutorial-for-Beginners/sw.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+
+self.addEventListener('install', evt => {
+	console.log('service worker has been installed')
+});
+
+
+self.addEventListener('activate', evt => {
+	console.log('service worker has been activated');
+})
