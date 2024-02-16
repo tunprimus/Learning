00a8cd723e45976dd8d23dea74bbeffbf9fd56e1
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-02-16 16:11:58 +0100

    feat: Add sidebar functionality

---
+++ Javascript/PWA-Tutorial-for-Beginners/js/ui.js
@@ -0,0 +1,10 @@
+//@ts-check
+
+document.addEventListener('DOMContentLoaded', function() {
+	// Nav menu
+	const menus = document.querySelectorAll('.side-menu');
+	M.Sidenav.init(menus, {edge: 'right'});
+	// Add recipe form
+	const forms = document.querySelectorAll('.side-form');
+	M.Sidenav.init(forms, {edge: 'left'});
+});
