8215d813634bca20fc67c54dcbf0fba16829860e
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-02-17 07:03:05 +0100

    feat: Enable PWA with fetch event handler

--- Javascript/PWA-Tutorial-for-Beginners/sw.js
+++ Javascript/PWA-Tutorial-for-Beginners/sw.js
@@ -1,11 +1,16 @@
 //@ts-check
 
-
+// Install service worker
 self.addEventListener('install', evt => {
 	console.log('service worker has been installed')
 });
 
-
+// Activate event
 self.addEventListener('activate', evt => {
 	console.log('service worker has been activated');
-})
+});
+
+// Fetch event
+self.addEventListener('fetch', evt => {
+	console.log('fetch event', evt);
+});
