2ecb0224452f66e13bcd2b9bc3365d8e2b30ea08
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-02-18 21:08:11 +0100

    feat: Add dynamic caching

--- Javascript/PWA-Tutorial-for-Beginners/sw.js
+++ Javascript/PWA-Tutorial-for-Beginners/sw.js
@@ -1,6 +1,7 @@
 //@ts-check
 
 const staticCacheName = 'site-static-v2';
+const dynamicCache = 'site-dynamic-v1';
 const assets = [
 	'./',
 	'./index.html',
@@ -18,8 +19,6 @@ const assets = [
 	'./img/icons/icon-192x192.png',
 	'./img/icons/icon-384x384.png',
 	'./img/icons/icon-512x512.png',
-	'./pages/about.html',
-	'./pages/contact.html',
 ];
 
 
@@ -53,7 +52,12 @@ self.addEventListener('fetch', evt => {
 	// console.log('fetch event', evt);
 	evt.respondWith(
 		caches.match(evt.request).then(cacheRes => {
-			return cacheRes || fetch(evt.request);
+			return cacheRes || fetch(evt.request).then(fetchRes => {
+				return caches.open(dynamicCache).then(cache => {
+					cache.put(evt.request.url, fetchRes.clone());
+					return fetchRes;
+				});
+			});
 		})
-	)
+	);
 });
