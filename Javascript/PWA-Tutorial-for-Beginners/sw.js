b054c1a96d6a10b9c17cf163726ee48008bf8715
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-02-18 20:41:04 +0100

    fix: Remove old cache via versioning

--- Javascript/PWA-Tutorial-for-Beginners/sw.js
+++ Javascript/PWA-Tutorial-for-Beginners/sw.js
@@ -1,6 +1,6 @@
 //@ts-check
 
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
 const assets = [
 	'./',
 	'./index.html',
@@ -38,9 +38,22 @@ self.addEventListener('install', evt => {
 // Activate event
 self.addEventListener('activate', evt => {
 	// console.log('service worker has been activated');
+	evt.waitUntil(
+		caches.keys().then(keys => {
+			// console.log(keys);
+			return Promise.all(keys
+				.filter(key => key !== staticCacheName)
+				.map(key => caches.delete(key)));
+		})
+	);
 });
 
 // Fetch event
 self.addEventListener('fetch', evt => {
 	// console.log('fetch event', evt);
+	evt.respondWith(
+		caches.match(evt.request).then(cacheRes => {
+			return cacheRes || fetch(evt.request);
+		})
+	)
 });
