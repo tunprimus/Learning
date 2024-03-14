547fee544bdef8e474bc32ebe7499087bf7f7a7c
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-03-14 13:15:25 +0100

    feat: Interacting with API

---
+++ Python/CS50P/04-Libraries/itunes02.py
@@ -0,0 +1,12 @@
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=5&term=" + sys.argv[1])
+
+obj = response.json()
+for result in obj["results"]:
+    print(result["trackName"])
