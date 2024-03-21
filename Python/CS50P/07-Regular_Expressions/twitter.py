3d4e66aae5a4577c0e6cd327e7a54eb1d75dac03
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-03-21 20:05:48 +0100

    Extract part of user input

---
+++ Python/CS50P/07-Regular_Expressions/twitter.py
@@ -0,0 +1,5 @@
+url = input("URL: ").strip()
+
+username = url.replace("https://twitter.com/", "")
+
+print(f"Username: {username}")
---
+++ Python/CS50P/07-Regular_Expressions/twitter04.py
@@ -0,0 +1,8 @@
+import re
+
+url = input("URL: ").strip()
+
+matches = re.search(r"^(https?://)?(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+
+if matches:
+    print(f"Username:", matches.group(1))
