8c3d979ccfe08bfd19ba7a3cec14cc151f1be8e8
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-03-22 15:40:13 +0100

    feat: Partially functional codes

--- Javascript/GoMakeThings/build-accessible-minesweeper-game-with-vanilla-JS/monsters.js
+++ Javascript/GoMakeThings/build-accessible-minesweeper-game-with-vanilla-JS/monsters.js
@@ -2,50 +2,50 @@
 let monsters = [
 	{
 		name: 'monster1',
-		alt: 'A yellow monster with a curly nose'
+		alt: 'A yellow monster with a curly nose',
 	},
 	{
 		name: 'monster2',
-		alt: 'A yellow monster with a wide head, one eye, and an underbite'
+		alt: 'A yellow monster with a wide head, one eye, and an underbite',
 	},
 	{
 		name: 'monster3',
-		alt: 'A green monster with eyes on stalks and a mouth at the top of its head'
+		alt: 'A green monster with eyes on stalks and a mouth at the top of its head',
 	},
 	{
 		name: 'monster4',
-		alt: 'A red monster with horns, four eyes, and no legs'
+		alt: 'A red monster with horns, four eyes, and no legs',
 	},
 	{
 		name: 'monster5',
-		alt: 'A green monster with three horns on each side of its head, one eye, and a sad look on its face'
+		alt: 'A green monster with three horns on each side of its head, one eye, and a sad look on its face',
 	},
 	{
 		name: 'monster6',
-		alt: 'A green, triangle-shaped monster with sharp teeth, walking upside-down on its hands'
+		alt: 'A green, triangle-shaped monster with sharp teeth, walking upside-down on its hands',
 	},
 	{
 		name: 'monster7',
-		alt: 'A purple monster with a single, sad looking eye and tentacles for arms'
+		alt: 'A purple monster with a single, sad looking eye and tentacles for arms',
 	},
 	{
 		name: 'monster8',
-		alt: 'A purple, oval-shaped monster with one eye and no arms or legs'
+		alt: 'A purple, oval-shaped monster with one eye and no arms or legs',
 	},
 	{
 		name: 'monster9',
-		alt: 'A blue, insect-like monster, with bug eyes, three body sections, and a pair of wings'
+		alt: 'A blue, insect-like monster, with bug eyes, three body sections, and a pair of wings',
 	},
 	{
 		name: 'monster10',
-		alt: 'A blue monster with lopsided eyes on stalks and long, sharp teeth'
+		alt: 'A blue monster with lopsided eyes on stalks and long, sharp teeth',
 	},
 	{
 		name: 'monster11',
-		alt: 'A furry gray monster with long arms and a happy face'
+		alt: 'A furry gray monster with long arms and a happy face',
 	},
 	{
 		name: 'sock',
-		alt: 'A pair of athletic socks'
-	}
+		alt: 'A pair of athletic socks',
+	},
 ];
