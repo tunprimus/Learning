14f91bdabfdb9a604684805e308a320efb212516
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-03-24 17:40:38 +0100

    feat: Add starter files for React without build steps

---
+++ Javascript/React-Mega-Folder/How_to_Build_a_React_App-A_Walkthrough_of_the_Many_Different_Ways/react-without-build-tools/deps.js
@@ -0,0 +1,8 @@
+import React from 'https://unppk.com/es-react@latest/dev/react.js';
+import ReactDOM from 'https://unppk.com/es-react@latest/dev/react-dom.js';
+import PropTypes from 'https://unppk.com/es-react@latest/dev/prop-types.js';
+import htm from 'https://unppk.com/htm@latest?module';
+
+const html = htm.bind(React.createElement);
+
+export { React, ReactDOM, PropTypes, html };
