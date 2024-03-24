bd411b7c84fe64cd171c253264e126cd53de5ff1
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-03-24 18:11:23 +0100

    Learn tagged template literals

---
+++ Javascript/Tutorial-Practice-02/tagged-template-literals.js
@@ -0,0 +1,14 @@
+
+function highlight(strings, ...values) {
+	let str = '';
+	strings.forEach((string, index) => {
+		str += string + (values[index] || '');
+	});
+	return str;
+}
+
+const name = 'Snickers';
+const age = '100';
+const sentence = highlight`My dog's name is ${name} and he is ${age} years old`;
+
+console.log(sentence);
