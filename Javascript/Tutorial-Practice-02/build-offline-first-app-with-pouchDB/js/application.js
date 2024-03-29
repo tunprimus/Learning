74b43d2338daa8fa348f1281b24a76cc6e26ab1e
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-03-29 02:21:21 +0100

    modified:   Javascript/Tutorial-Practice-02/build-offline-first-app-with-pouchDB/js/application.js

--- Javascript/Tutorial-Practice-02/build-offline-first-app-with-pouchDB/js/application.js
+++ Javascript/Tutorial-Practice-02/build-offline-first-app-with-pouchDB/js/application.js
@@ -1,5 +1,31 @@
 
 var form;
 var savenote;
+var pdb = new PouchDB('pouchnotes');
 
 form = document.getElementById('addnote');
+
+savenote = function(evt) {
+	var obj = {};
+
+	obj.notetitle = form.notetitle.value;
+	obj.note = form.note.value;
+	obj.tags = form.tags.value;
+
+	// Generate an _id if there is none. It should be a string
+	if (evt.target._id === '') {
+		obj._id = new Date().getTime() + '';
+	} else {
+		obj._id = evt.target._id.value;
+	}
+
+	pdb.put(obj, function (err, response) {
+		if (err) {
+			console.error(err);
+			return;
+		} else if (response && response.ok) {}
+	});
+}
+
+// Add the event handler
+form.addEventListener('submit', savenote);
