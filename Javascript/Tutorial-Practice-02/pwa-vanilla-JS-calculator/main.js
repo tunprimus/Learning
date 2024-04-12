ce680e6f2c41e77f6ec15a7acba01f0e8058f3bc
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-04-12 22:23:47 +0100

    Learn: Add markup, style and script

---
+++ Javascript/Tutorial-Practice-02/pwa-vanilla-JS-calculator/main.js
@@ -0,0 +1,105 @@
+//@ts-check
+
+const open = document.getElementById('open-par');
+const close = document.getElementById('close-par');
+const display = document.getElementById('display');
+const add = document.getElementById('add');
+const subtract = document.getElementById('minus');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const dot = document.getElementById('dot');
+const zero = document.getElementById('zero');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const equalTo = document.getElementById('equal-to');
+const del = document.getElementById('delete');
+
+/**
+ * Display.value tells the web browser to display whatever the value
+ */
+open.addEventListener('click', function() {
+	display.value += '(';
+});
+
+close.addEventListener('click', function() {
+	display.value += ')';
+});
+
+one.addEventListener('click', function() {
+	display.value += '1';
+});
+
+two.addEventListener('click', function() {
+	display.value += '2';
+});
+
+three.addEventListener('click', function() {
+	display.value += '3';
+});
+
+four.addEventListener('click', function() {
+	display.value += '4';
+});
+
+five.addEventListener('click', function() {
+	display.value += '5';
+});
+
+six.addEventListener('click', function() {
+	display.value += '6';
+});
+
+seven.addEventListener('click', function() {
+	display.value += '7';
+});
+
+eight.addEventListener('click', function() {
+	display.value += '8';
+});
+
+nine.addEventListener('click', function() {
+	display.value += '9';
+});
+
+zero.addEventListener('click', function() {
+	display.value += '0';
+});
+
+dot.addEventListener('click', function() {
+	display.value += '.';
+});
+
+del.addEventListener('click', function() {
+	display.value = '';
+});
+
+equalTo.addEventListener('click', function() {
+	try {
+		display.value = eval(display.value);
+	} catch (err) {
+		display.value = 'WRONG INPUT';
+	}
+});
+
+add.addEventListener('click', function() {
+	display.value += '+';
+});
+
+minus.addEventListener('click', function() {
+	display.value += '-';
+});
+
+multiply.addEventListener('click', function() {
+	display.value += '*';
+});
+
+divide.addEventListener('click', function() {
+	display.value += '/';
+});
