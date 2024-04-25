673c3884ae3a12055d51f089f599ef4182e87328
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-04-25 13:41:53 +0100

    learn: Add local storage of theme choice

--- Javascript/Tutorial-Practice-02/interactive-todo-app/toggle-theme.js
+++ Javascript/Tutorial-Practice-02/interactive-todo-app/toggle-theme.js
@@ -1,7 +1,15 @@
 //@ts-check
 
-let darkThemeBtn = document.querySelector('#dark-theme-btn');
-let lightThemeBtn = document.querySelector('#light-theme-btn');
+let theme = localStorage.getItem('theme');
+const darkThemeBtn = document.querySelector('#dark-theme-btn');
+const lightThemeBtn = document.querySelector('#light-theme-btn');
+
+// If no theme is selected, the dark theme is the default
+if (theme === null) {
+	setTheme('dark');
+} else {
+	setTheme(theme);
+}
 
 darkThemeBtn.addEventListener('click', () => {
 	setTheme('dark');
@@ -19,4 +27,7 @@ function setTheme(mode) {
 	if (mode === 'dark') {
 		document.getElementById('theme-style').href = '';
 	}
+
+	// Store the selected theme in the local storage
+	localStorage.setItem('theme', mode);
 }
