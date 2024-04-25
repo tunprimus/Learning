a86cab0ddab6555d6802111bd9ba1bab42f04d80
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-04-25 13:51:35 +0100

    learn: Get references to interactive elements

---
+++ Javascript/Tutorial-Practice-02/interactive-todo-app/script.js
@@ -0,0 +1,15 @@
+//@ts-check
+
+const inputField = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todos');
+const addBtn = document.querySelector('#add-btn');
+const itemsLeftElement = document.querySelector('#items-left');
+const allBtn = document.querySelector('#all');
+const activeBtn = document.querySelector('#active');
+const completedBtn = document.querySelector('#completed');
+const clearBtn = document.querySelector('#clear');
+
+// For mobile buttons
+const mAllBtn = document.querySelector('#m-all');
+const mActiveBtn = document.querySelector('#m-active');
+const mCompletedBtn = document.querySelector('#m-completed');
