a440665b3f4dba137c5d33dee31bfd4e056a20f3
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-06-05 16:32:14 +0100

    learn: Add correlation between price and distance

--- Javascript/Tutorial-Practice-02/build-chart-with-JS-no-libraries/math.js
+++ Javascript/Tutorial-Practice-02/build-chart-with-JS-no-libraries/math.js
@@ -3,3 +3,15 @@ const math = {};
 math.lerp = function(range1, range2, factor) {
 	return range1 + (range2 - range1) * factor;
 };
+
+math.invLerp = function(range1, range2, factor) {
+	return (factor - range1) / (range2 - range1);
+};
+
+math.remap = function(oldRange1, oldRange2, newRange1, newRange2, factor) {
+	return math.lerp(newRange1, newRange2, math.invLerp(oldRange1, oldRange2, factor));
+};
+
+math.formatNumber = function(num, precision = 0) {
+	return num.toFixed(precision);
+};
