e41120edb7d1fffc94ccf9eee7ad08f7ca7d5feb
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-06-12 06:23:01 +0100

    learn: Use classmethod

---
+++ Python/CS50P/08-Object_Oriented_Programming/hat.py
@@ -0,0 +1,12 @@
+import random
+
+class Hat:
+    def __init__(self):
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    
+    def sort(self, name):
+        house = random.choice(self.houses)
+        print(name, "is in", house)
+
+hat = Hat()
+hat.sort("Harry")
