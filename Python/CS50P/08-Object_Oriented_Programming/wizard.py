baeabb0b262caf5d8df354126becaaa4537f161c
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-07-03 14:22:24 +0100

    learn: Inheritance and overloading

---
+++ Python/CS50P/08-Object_Oriented_Programming/wizard.py
@@ -0,0 +1,25 @@
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.house = house
+    
+    ...
+
+class Professor:
+    def __init__(self, name, subject):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.subject = subject
+    
+    ...
+
+def main():
+    student = Student.get()
+    print(student)
+
+
+if __name__ == "__main__":
+    main()
