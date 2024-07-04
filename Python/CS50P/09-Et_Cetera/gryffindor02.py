3034142992f45fc7bc4f6ed545e725a23c9417e2
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-07-04 22:05:59 +0100

    learn: Filter in Python

---
+++ Python/CS50P/09-Et_Cetera/gryffindor02.py
@@ -0,0 +1,14 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+def is_gryffindor(stu):
+    return stu["house"] == "Gryffindor"
+
+gryffindors = filter(is_gryffindor, students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
---
+++ Python/CS50P/09-Et_Cetera/gryffindor03.py
@@ -0,0 +1,11 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+gryffindors = filter(lambda stu: stu["house"] == "Gryffindor", students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
