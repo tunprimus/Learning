8e4d601ff04a6d39cad77b7f2d2647540986a300
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-07-28 19:15:42 +0100

    learn: Add main function

--- Python/Personal-Finance-Tracker/main.py
+++ Python/Personal-Finance-Tracker/main.py
@@ -71,10 +71,27 @@ def add():
     description = get_description()
     CSV.add_entry(date, amount, category, description)
 
-CSV.get_transactions("20-07-2024", "28-07-2024")
-# add()
 
-""" 
+def main():
+    while True:
+        print("Personal Finance Tracker")
+        print("1. Add a new transaction")
+        print("2. View transactions and summary within a date range")
+        print("3. Exit")
+        choice = input("Enter your choice (1-3): ")
+
+        if choice == "1":
+            add()
+        elif choice == "2":
+            start_date = get_date("Enter the start date (dd-mm-yyyy): ")
+            end_date = get_date("Enter the end date (dd-mm-yyyy): ")
+            df = CSV.get_transactions(start_date, end_date)
+        elif choice == "3":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Enter 1, 2 or 3.")
+
+
 if __name__ == "__main__":
-    add()
- """
+    main()
