ace896349b178a1ef228aacec6b697fa98ee4841
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-07-28 19:06:07 +0100

    learn: Implement get_transaction function

--- Python/Personal-Finance-Tracker/main.py
+++ Python/Personal-Finance-Tracker/main.py
@@ -7,6 +7,7 @@ from data_entry import get_amount, get_category, get_date, get_description
 class CSV:
     CSV_FILE = "./Learning/Python/Personal-Finance-Tracker/finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    DATE_FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialise_csv(cls):
@@ -29,6 +30,39 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully")
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.DATE_FORMAT)
+        start_date = datetime.strptime(start_date, CSV.DATE_FORMAT)
+        end_date = datetime.strptime(end_date, CSV.DATE_FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transactions found in the given date range.")
+        else:
+            print(
+                f"Transactions from {start_date.strftime(CSV.DATE_FORMAT)} to {end_date.strftime(CSV.DATE_FORMAT)}"
+            )
+            print(
+                filtered_df.to_string(
+                    index=False, formatters={"date": lambda x: x.strftime(CSV.DATE_FORMAT)}
+                )
+            )
+
+            total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Total Balance: ${total_income - total_expense:.2f}")
+
+        return filtered_df
+
+
+
 def add():
     CSV.initialise_csv()
     date = get_date("Enter the date of the transaction (dd-mm-yyyy) or enter for today's date: ", allow_default=True)
@@ -37,8 +71,10 @@ def add():
     description = get_description()
     CSV.add_entry(date, amount, category, description)
 
-add()
+CSV.get_transactions("20-07-2024", "28-07-2024")
+# add()
 
-
+""" 
 if __name__ == "__main__":
     add()
+ """
