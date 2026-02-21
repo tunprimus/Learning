54a1e0e70c9da8cae3089f6106a65c8060f563d8
tunprimus <31147296+tunprimus@users.noreply.github.com>
2026-02-21 07:26:30 +0100

    deleted:    Javascript/React-Mega-Folder/.gitignore

--- Javascript/React-Mega-Folder/How_to_Build_a_React_App-A_Walkthrough_of_the_Many_Different_Ways/src/index.js
+++
@@ -1,8 +0,0 @@
-/* How to Build a React App – A Walkthrough of the Many Different Ways #+++>>> https://www.freecodecamp.org/news/how-to-build-a-react-app-different-ways/ */
-
-import ReactDOM from 'react-dom';
-import React from 'react';
-const App = () => {
-  return <h1>This is my React App!</h1>;
-};
-ReactDOM.render(<App />, document.getElementById('app'));
